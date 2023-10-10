25be26420c84489ec9c9e5f39ce97de6bd059a81
Vladislav Sungurov <vladislavsunny@gmail.com>
2023-10-10 12:28:02 +0300

    First Launch

---
+++ lexicon/lexicon.py
@@ -0,0 +1,6 @@
+LEXICON: dict[str, dict[str, str]] = {
+    'default_response': {
+        'en': "Sorry, I didn't recognize your request. Can I help you with something else?",
+        'ru': "Извините, я не распознал ваш запрос. Могу помочь чем-то еще?"
+    }
+}
