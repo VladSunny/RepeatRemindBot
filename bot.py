2955caa6881ec1842e5bcb82e08f9a04618a5c60
Vladislav Sungurov <vladislavsunny@gmail.com>
2023-10-14 09:09:07 +0300

    Create requirements.txt First deploy

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ async def main():
 
     # Пропускаем накопившиеся апдейты и запускаем polling
     await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+    await dp.start_polling(bot, allowed_updates=[])
 
 
 if __name__ == '__main__':
