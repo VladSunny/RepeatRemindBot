74bd645478d84d1d5260f1b1f5ff9d914de8f906
vladg00dman <vladislavsunny@gmail.com>
2023-12-20 17:12:13 +0300

    User restrictions update

--- config_data/user_restrictions.py
+++ config_data/user_restrictions.py
@@ -1,8 +1,8 @@
 # конфиг со всемы иограничениями пользователя
 
 max_modules = 5
-max_items_in_module = 15
-max_local_items_in_module = 40
+max_items_in_module = 30
+max_local_items_in_module = 50
 
 max_name_length = 20
 max_element_length = 60
