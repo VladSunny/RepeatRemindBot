c223aa4c3ec12725bfc4c8e6da32f22b023bcc02
Vlad Sunny <vladislavsunny@gmail.com>
2024-03-19 21:09:05 +0300

    Faster translation!

--- services/auto_translate_service.py
+++ services/auto_translate_service.py
@@ -11,8 +11,11 @@ def translate_phrase(phrase: str, from_lang: str, to_lang: str) -> str:
 def translate_all_phrases_into_module_pairs(phrases: list[str]) -> dict[str, str]:
     items = {}
 
-    for i in phrases:
-        items[i] = translate_phrase(i, 'en', 'ru').strip().lower()
+    phrases_line = '. '.join(phrases)
+    translated_phrases: list[str] = translate_phrase(phrases_line, 'en', 'ru').strip().lower().split('. ')
+
+    for i in range(len(phrases)):
+         items[phrases[i]] = translated_phrases[i]
 
     return items
 
