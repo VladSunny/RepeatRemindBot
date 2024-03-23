93992b13bcce26350b8b1c12fb6775bfc52ed482
Vlad Sunny <vladislavsunny@gmail.com>
2024-03-23 12:46:01 +0300

    Get score

--- handlers/game_for_module_handler.py
+++ handlers/game_for_module_handler.py
@@ -46,6 +46,7 @@ async def query_inline_game(callback_query: types.CallbackQuery, bot: Bot, state
 
     # Базовый URL игры
     game_url = f'https://repeatclicker.netlify.app/repeat-remind-game?module_id={data["module_id"]}'
+    game_url += f'&user_id={user_id}'
 
     # Для inline сообщений добавляем inline_message_id, иначе добавляем chat_id
     if callback_query.inline_message_id:
